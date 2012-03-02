7b20e418f88ab6611ef5794e8cb00de329dd4661
Ian Henry <ianthehenry@gmail.com>
2012-03-02 13:26:06 +0600

    Cleaned up the reader; added smarter __repr__ methods to FormToken and PairToken; added reading numeric literals; added [a | b] syntax

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from itertools import takewhile
 
 nil = None
 class Nil:
@@ -10,11 +11,25 @@ class Nil:
         return 'nil'
 nil = Nil()
 
+Pair = tuple
+
+PAIR_CDR_TOKEN = '|'
+
 def tuplify(lst):
+    if type(lst) is not tuple:
+        return lst
     if len(lst) == 0:
         return nil
-    if type(lst) is not list:
-        return lst
+
+    if lst[0] == PAIR_CDR_TOKEN: # [| b]
+        raise Exception("cannot have a cdr without a car")
+    if len(lst) > 1 and lst[1] == PAIR_CDR_TOKEN:
+        if len(lst) == 2: # [a |]
+            raise Exception("no cdr specified after explicit cdr")
+        if len(lst) > 3: # [a | b c]
+            raise Exception("can only specify one cdr")
+        return (lst[0], lst[2])
+
     return (tuplify(lst[0]), tuplify(lst[1:]))
 
 def lex(code): # converts string to tokens, currently represented as simple strings
@@ -34,81 +49,93 @@ def lex(code): # converts string to tokens, currently represented as simple stri
             current_token += c
     return tokens
 
-def split(lst, predicate):
-    first = []
-    second = []
-    i = 0
-    while i < len(lst):
-        item = lst[i]
-        i += 1
-        if predicate(item):
-            break
-        else:
-            first.append(item)
+def continue_repr(tup, closing_symbol):
+    assert len(tup) == 2
+    car, cdr = tup
+
+    if type(cdr) is Pair:
+        return "%s %s" % (repr(car), continue_repr(cdr, closing_symbol))
+    elif cdr is nil:
+        return "%s%s" % (repr(car), closing_symbol)
     else:
-        raise Exception('Invalid split: predicate never resolved to true')
-    while i < len(lst):
-        second.append(lst[i])
-        i += 1
-    return (first, second)
+        return "%s | %s%s" % (repr(car), repr(cdr), closing_symbol)
 
 class FormToken:
-    def __init__(self, sexp):
-        if type(sexp) is list:
-            self.sexp = tuplify(sexp)
-        else:
-            self.sexp = sexp
-        self.car = sexp[0]
-        self.cdr = sexp[1]
+    def __init__(self, *sexp):
+        self.sexp = tuplify(sexp)
+        self.car = self.sexp[0]
+        self.cdr = self.sexp[1]
     def __repr__(self):
-        return 'F' + repr(self.sexp)
+        if type(self.cdr) is Pair:
+            return "(%s %s" % (repr(self.car), continue_repr(self.cdr, ')'))
+        elif self.cdr is nil:
+            return "%s)" % self.car
+        else:
+            return "(%s | %s)" % (repr(self.car), repr(self.cdr))
     def __len__(self):
         return len(self.sexp)
     def __eq__(self, other):
         return type(other) is FormToken and self.sexp == other.sexp
 
 class PairToken:
-    def __init__(self, sexp):
-        if type(sexp) is list:
-            self.sexp = tuplify(sexp)
-        else:
-            self.sexp = sexp
-        self.car = sexp[0]
-        self.cdr = sexp[1]
+    def __init__(self, *sexp):
+        self.sexp = tuplify(sexp)
+        self.car = self.sexp[0]
+        self.cdr = self.sexp[1]
     def __repr__(self):
-        return 'P' + repr(self.sexp)
+        if type(self.cdr) is Pair:
+            return "[%s %s" % (repr(self.car), continue_repr(self.cdr, ']'))
+        elif self.cdr is nil:
+            return "%s]" % self.car
+        else:
+            return "[%s | %s]" % (repr(self.car), repr(self.cdr))
     def __len__(self):
         return len(self.sexp)
     def __eq__(self, other):
         return type(other) is PairToken and self.sexp == other.sexp
 
-def read_matched_code(tokens, start, end, constructor):
+def read_matched_code(tokens, start, end, constructor, empty_constructor):
     nesting_depth = 1
     def pred(token):
         nonlocal nesting_depth
         if token == start:
             nesting_depth += 1
-            return False
-        if token == end:
+        elif token == end:
             nesting_depth -= 1
-            return nesting_depth == 0
+            return nesting_depth > 0
+        return True
 
-    form_tokens, remaining_tokens = split(tokens, pred)
-    return (constructor((form_tokens[0], read(form_tokens[1:]))), read(remaining_tokens))
+    form_tokens = list(takewhile(pred, tokens))
+
+    if len(form_tokens) == 0:
+        return (empty_constructor(), 1)
+
+    return (constructor(*read(form_tokens)), len(form_tokens) + 1) # +1 to account for the closing paren/bracket/whatever
+
+def is_token_numeric_literal(token):
+    return re.match(r'\d+', token)
+
+def read_single_token(token):
+    if re.match(r'\d+', token):
+        return int(token)
+    return token
 
 matched_tokens = {
-    '(': (')', FormToken),
-    '[': (']', PairToken)
+    '(': (')', FormToken, lambda: nil), # this should actually throw an exception, but i'm allowing it for now...makes sense if nil can be used as a function
+    '[': (']', PairToken, lambda: nil)
 }
 
 def read(tokens): # converts token stream to s-expressions, parses numeric literals, etc
     if len(tokens) == 0:
-        return nil
-    if tokens[0] in matched_tokens:
-        opening = tokens[0]
-        return read_matched_code(tokens[1:], opening, *matched_tokens[opening])
+        return []
 
-    return (tokens[0], read(tokens[1:]))
+    first = tokens[0]
+
+    if first in matched_tokens:
+        matched_token, tokens_consumed = read_matched_code(tokens[1:], first, *matched_tokens[first])
+        return [matched_token] + read(tokens[1 + tokens_consumed:])
+
+    return [read_single_token(first)] + read(tokens[1:])
 
 def isheval(statement, scope):
     assert statement is not nil
@@ -122,14 +149,18 @@ def isheval(statement, scope):
 code = '''
 (print (add 5 6))
 (print [7 | 8])
+(print [(add 10 20)])
 '''
 
 actual = read(lex(code))
-expected = tuplify([
-    FormToken(['print', FormToken(['add', '5', '6'])]),
-    FormToken(['print', PairToken(['7', '|', '8'])])
-])
+expected = [
+    FormToken('print', FormToken('add', 5, 6)),
+    FormToken('print', PairToken(7, '|', 8)),
+    FormToken('print', PairToken(FormToken('add', 10, 20))),
+]
 
+print('; '.join(map(repr, actual)))
+print('; '.join(map(repr, expected)))
 assert(actual == expected)
 
 class Scope:
@@ -146,8 +177,8 @@ class Scope:
 scope = Scope({
     'print': lambda arg, scope: print(isheval(arg[0], scope)),
     'add': lambda arg, scope: arg[0] + arg[1][0],
+    'id': lambda arg, scope: arg[0],
     'nil': nil
 }, None)
 
-print(actual)
 isheval(actual[0], scope)

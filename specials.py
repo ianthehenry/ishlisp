ca6ee49e60c7d7b52d93d803ee3e7765b7e6c61c
Ian Henry <ianthehenry@gmail.com>
2012-03-02 22:49:51 +0600

    make sure you evaluate the explicit cdr in a list special form

--- specials.py
+++ specials.py
@@ -3,6 +3,7 @@ from reader import FormNode, nil, IdentifierNode, Node
 
 def print_(arg, scope):
     print(eval_node(arg.car, scope))
+    return nil
 
 def add(arg, scope):
     return eval_node(arg.car, scope) + eval_node(arg.cdr.car, scope)
@@ -21,7 +22,7 @@ def list_(arg, scope):
         return nil
     if type(arg) is not FormNode:
         raise Exception('cannot have a cdr without a car')
-    return Pair(eval_node(arg.car, scope), list_(arg.cdr, scope) if type(arg.cdr) is FormNode else arg.cdr)
+    return Pair(eval_node(arg.car, scope), list_(arg.cdr, scope) if type(arg.cdr) is FormNode else eval_node(arg.cdr, scope))
 
 def fn(arg, scope):
     param_name = arg.car
